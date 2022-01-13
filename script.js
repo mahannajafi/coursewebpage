a401096c804cb16ae7dcdc968d63b60ed5962efc
mahannajafi <mahan.nwork@gmail.com>
2022-01-13 04:37:33 +0330

    header and login,register

---
+++ script.js
@@ -0,0 +1,47 @@
+
+// **********humberg menu**************
+const menu=document.querySelector('.fa-bars')
+const menuOptions=document.querySelector('.navbar__container')
+const menuClose=document.querySelector('.header__close')
+menu.addEventListener('click',()=>{
+  menuOptions.classList.add('navbar__container--show')
+  menuClose.classList.add('flex')
+}
+)
+menuClose.addEventListener('click',()=>{
+  menuOptions.classList.remove('navbar__container--show')
+  menuClose.classList.remove('flex')
+})
+
+//****************login form *********************
+const user=document.querySelector('.fa-user')
+const userAdd=document.querySelector('.form')
+const userClose=document.querySelector('.form__close')
+
+user.addEventListener('click',()=>{
+  userAdd.style.display='inline'
+})
+
+userClose.addEventListener('click',()=>{
+  userAdd.style.display='none'
+})
+
+const login=document.querySelectorAll('.form__button')
+const loginShow=document.querySelector('.form__login')
+const registerShow=document.querySelector('.form__register')
+const loginArray=[...login]
+console.log(loginArray);
+loginArray[0].addEventListener('click',()=>{
+  loginShow.style.display='inline'
+  registerShow.style.display='none'
+  loginArray[0].classList.add('form__button--active')
+  loginArray[1].classList.remove('form__button--active')
+
+})
+loginArray[1].addEventListener('click',()=>{
+  loginShow.style.display='none'
+  registerShow.style.display='inline'
+  loginArray[1].classList.add('form__button--active')
+  loginArray[0].classList.remove('form__button--active')
+
+})
